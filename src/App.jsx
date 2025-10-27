9786d37cd802d8d11a5d98b654a2684fb7e60c4c
Your Name <evangelista.luther1995@gmail.com>
2025-10-27 17:08:15 +1000

    Added starter files and implemented page navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from './pages/Homepage';
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
-import AppLayout from './pages/AppLayout';
+import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
         <Route path='/' element={<Homepage />} />
         <Route path='product' element={<Product />} />
         <Route path='pricing' element={<Pricing />} />
-        <Route path='app' element={<AppLayout />} />
+        <Route path='login' element={<Login />} />
         <Route path='*' element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
