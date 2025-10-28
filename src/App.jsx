4ee9d3d9be4f624a687997eca749c09eb1c8907c
Your Name <evangelista.luther1995@gmail.com>
2025-10-28 15:54:37 +1000

    feat(route): implement useNavigate hook on Map to form navigation and Form back button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState, useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
-import Login from './pages/Login'
-import PageNotFound from './pages/PageNotFound'
+import Login from './pages/Login';
+import PageNotFound from './pages/PageNotFound';
 import AppLayout from './pages/AppLayout';
-import CityList from './components/CityList'
-import City from "./components/City";
+import CityList from './components/CityList';
+import City from './components/City';
+import Form from './components/Form';
 import CountryList from './components/CountryList';
 
 const BASE_URL = 'http://localhost:8000';
@@ -16,10 +17,10 @@ function App() {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(function() {
+  useEffect(function () {
     async function fetchCities() {
       try {
-        setIsLoading(true)
+        setIsLoading(true);
         const res = await fetch(`${BASE_URL}/cities`);
         const data = await res.json();
         setCities(data);
@@ -32,24 +33,33 @@ function App() {
     fetchCities();
   }, []);
 
-  return(
+  return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Homepage />} />
-        <Route path='product' element={<Product />} />
-        <Route path='pricing' element={<Pricing />} />
-        <Route path='login' element={<Login />} />
-        <Route path='app' element={<AppLayout />}>
-          <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
-          <Route path='cities' element={<CityList cities={cities} isLoading={isLoading} />} />
-          <Route path='cities/:id' element={<City />} />
-          <Route path='countries' element={<CountryList cities={cities} isLoading={isLoading}/>} />
-          <Route path='form' element={<p>Form</p>} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="cities/:id" element={<City />} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="form" element={<Form />} />
         </Route>
-        <Route path='*' element={<PageNotFound />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App
+export default App;
