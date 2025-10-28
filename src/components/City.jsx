ea1d60b2e575c952b41d47aadb952a740938ce6a
Your Name <evangelista.luther1995@gmail.com>
2025-10-28 15:08:48 +1000

    feat(route): integrate useSearchParams hook for lat/lng in Map and City

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,30 +1,36 @@
-import styles from "./City.module.css";
-import { useParams } from "react-router-dom";
+import styles from './City.module.css';
+import { useParams, useSearchParams } from 'react-router-dom';
 
 const formatDate = (date) =>
-  new Intl.DateTimeFormat("en", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-    weekday: "long",
+  new Intl.DateTimeFormat('en', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+    weekday: 'long',
   }).format(new Date(date));
 
 function City() {
-  const id = useParams()
+  const id = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
 
   // TEMP DATA
   const currentCity = {
-    cityName: "Lisbon",
-    emoji: "🇵🇹",
-    date: "2027-10-31T15:59:59.138Z",
-    notes: "My favorite city so far!",
+    cityName: 'Lisbon',
+    emoji: '🇵🇹',
+    date: '2027-10-31T15:59:59.138Z',
+    notes: 'My favorite city so far!',
   };
 
   const { cityName, emoji, date, notes } = currentCity;
 
   return (
     <div className={styles.city}>
-      <h1>id</h1>
+      <h1>
+        Position: {lat}, {lng}
+      </h1>
+
       {/* <div className={styles.row}>
         <h6>City name</h6>
         <h3>
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,26 @@
+import { useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
 
 function Map() {
-    return (
-        <div className={styles.mapContainer}>
-            <p>Map</p>
-        </div>
-    );
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
+
+  return (
+    <div className={styles.mapContainer}>
+      <p>Map</p>
+      <h1>
+        Position: {lat}, {lng}
+      </h1>
+      <button
+        onClick={() => {
+          setSearchParams({ lat: 23, lng: 50 });
+        }}
+      >
+        Change position
+      </button>
+    </div>
+  );
 }
 
 export default Map;
